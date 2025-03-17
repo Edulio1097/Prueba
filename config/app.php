e4a0fa79b957081f43fc867172da9d7540851bf5
Edulio1097 <edulio1097@gmail.com>
2025-03-16 19:26:32 -0500

    Prueba .sizas3

--- config/app.php
+++ config/app.php
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://prueba-57gb.onrender.com'),
 
     'asset_url' => env('ASSET_URL'),
 
